830540b0f648cec493e9eaaa4f716d32e0be5e9b
gurisinghparmar <guriparmar3@gmail.com>
2015-12-10 21:28:47 -0500

    updates
    updates

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -18,8 +18,7 @@ class UsersController extends AppController
      */
     public function index()
     {
-        $this->set('users', $this->paginate($this->Users));
-        $this->set('_serialize', ['users']);
+       $this->set('users', $this->Users->find('all'));
     }
 
     /**
@@ -29,13 +28,10 @@ class UsersController extends AppController
      * @return void
      * @throws \Cake\Network\Exception\NotFoundException When record not found.
      */
-    public function view($id = null)
+    public function view($id )
     {
-        $user = $this->Users->get($id, [
-            'contain' => []
-        ]);
-        $this->set('user', $user);
-        $this->set('_serialize', ['user']);
+        $user = $this->Users->get($id);
+        $this->set(compact('user'));
     }
 
     /**
@@ -45,18 +41,16 @@ class UsersController extends AppController
      */
     public function add()
     {
-        $user = $this->Users->newEntity();
+       $user = $this->Users->newEntity();
         if ($this->request->is('post')) {
             $user = $this->Users->patchEntity($user, $this->request->data);
             if ($this->Users->save($user)) {
                 $this->Flash->success(__('The user has been saved.'));
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The user could not be saved. Please, try again.'));
+                return $this->redirect(['action' => 'add']);
             }
+            $this->Flash->error(__('Unable to add the user.'));
         }
-        $this->set(compact('user'));
-        $this->set('_serialize', ['user']);
+        $this->set('user', $user);
     }
 
     /**
@@ -83,7 +77,30 @@ class UsersController extends AppController
         $this->set(compact('user'));
         $this->set('_serialize', ['user']);
     }
+    public function beforeFilter(Event $event)
+    {
+       parent::beforeFilter($event);
+    // Allow users to register and logout.
+    // You should not add the "login" action to allow list. Doing so would
+    // cause problems with normal functioning of AuthComponent.
+    $this->Auth->allow(['add', 'logout']);
+    }
+public function login()
+{
+    if ($this->request->is('post')) {
+        $user = $this->Auth->identify();
+        if ($user) {
+            $this->Auth->setUser($user);
+            return $this->redirect($this->Auth->redirectUrl());
+        }
+        $this->Flash->error(__('Invalid username or password, try again'));
+    }
+}
 
+    public function logout()
+{
+    return $this->redirect($this->Auth->logout());
+}
     /**
      * Delete method
      *
--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -16,17 +16,17 @@ use Cake\ORM\Entity;
 class User extends Entity
 {
 
-    /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * Note that when '*' is set to true, this allows all unspecified fields to
-     * be mass assigned. For security purposes, it is advised to set '*' to false
-     * (or remove it), and explicitly make individual fields accessible as needed.
-     *
-     * @var array
-     */
+    // Make all fields mass assignable except for primary key field "id".
     protected $_accessible = [
         '*' => true,
-        'id' => false,
+        'id' => false
     ];
+
+    // ...
+
+    protected function _setPassword($password)
+    {
+        return (new DefaultPasswordHasher)->hash($password);
+    }
+
 }
